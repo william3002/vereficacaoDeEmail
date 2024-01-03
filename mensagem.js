ff58792a0d96543996c03123ecda47404d0b2d90
William Araujo <106315401+william3002@users.noreply.github.com>
2024-01-02 21:47:48 -0300

    Email
    Email digitado pelo o usuario aparece na janela de mensagem

--- mensagem.js
+++ mensagem.js
@@ -2,8 +2,10 @@ function campoVazio() {
     let Email = document.getElementById('inputEmail').value
     let errorSpan = document.getElementById('error')
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let emailUser = document.getElementById('emailUser')
 
-
+    emailUser.innerText = Email
+    
     if (Email === "") {
         errorSpan.innerText = "Por favor, prencha este campo de email"
         errorSpan.style.fontSize = 'small'
@@ -13,8 +15,8 @@ function campoVazio() {
         document.getElementById('mensagem-sucesso').style.display = 'none';
 
         return;
-    } 
-    
+    }
+
     if (!emailRegex.test(Email)) {
         errorSpan.innerText = "Por favor, Insira e-mail válido.";
         errorSpan.style.fontSize = 'small';
@@ -27,10 +29,10 @@ function campoVazio() {
 
 }
 
-function btnFechaJanela(){
+function btnFechaJanela() {
     let principal = document.getElementById('principal')
     let mensagemSucesso = document.getElementById('mensagem-sucesso')
-    if(principal && mensagemSucesso){
+    if (principal && mensagemSucesso) {
         principal.style.display = "block"
         mensagemSucesso.style.display = "none"
     }
